bbd5fc3ed7e16032b03d41922ea907c5960866c0
itsjustyuji <vedantsharmavs2002@gmail.com>
2025-09-03 14:57:16 +0530

    Home page completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <AiTools />
+      <Plan />
+      <Footer />
     </>
   )
 }
